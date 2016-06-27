0a739ee6a557c6f8a741e8a088d761f2342bf57a
Alex Henrie <alexhenrie24@gmail.com>
2016-06-26 19:44:35 -0600

    Print names of cases without the segment and controls with the segment

--- proband-linkage.py
+++ proband-linkage.py
@@ -25,8 +25,11 @@
 import csv
 import glob
 import sys
+from copy import copy
 from getopt import getopt
 from math import sqrt
+from os.path import basename
+from os.path import splitext
 from scipy.stats import norm
 
 case_dir = 'cases'
@@ -34,8 +37,9 @@ control_dir = 'controls'
 proband_is_case = None
 alpha = 0.05
 m = 23
+want_misfits = True
 
-optlist, args = getopt(sys.argv[1:], '-a', ['cases=', 'controls=', 'proband=', 'alpha=', 'no-bonferroni', 'help'])
+optlist, args = getopt(sys.argv[1:], '-a', ['cases=', 'controls=', 'proband=', 'alpha=', 'no-bonferroni', 'no-misfit-names', 'help'])
 for name, value in optlist:
     if name == '--cases':
         case_dir = value
@@ -50,8 +54,10 @@ for name, value in optlist:
         alpha = float(value)
     elif name == '--no-bonferroni':
         m = 1
+    elif name == '--no-misfit-names':
+        want_misfits = False
     elif name == '--help':
-        print('Syntax: ./proband-linkage.py [--cases=<dir>] [--controls=<dir>] [--proband=(case|control)] [--alpha=<value>] [--no-bonferroni]')
+        print('Syntax: ./proband-linkage.py [--cases=<dir>] [--controls=<dir>] [--proband=(case|control)] [--alpha=<value>] [--no-bonferroni] [--no-misfit-names]')
         print('cases defaults to ./cases, controls defaults to ./controls, and alpha defaults to 0.05.')
         exit(1)
 
@@ -67,52 +73,57 @@ while proband_is_case == None:
 def create_start_point(segments, point):
     for i in range(0, len(segments)):
         if segments[i][0] < point and point < segments[i][1]:
-            #shrink the segment and insert a new segment after it with the same totals
-            segments.insert(i + 1, [point, segments[i][1], segments[i][2], segments[i][3]])
+            #shrink the segment and insert a new segment after it with the same people
+            segments.insert(i + 1, [point, segments[i][1], copy(segments[i][2]), copy(segments[i][3])])
             segments[i][1] = point - 1
             break
 
 def create_end_point(segments, point):
     for i in range(0, len(segments)):
         if segments[i][0] < point and point < segments[i][1]:
-            #shrink the segment and insert a new segment before it with the same totals
-            segments.insert(i, [segments[i][0], point, segments[i][2], segments[i][3]])
+            #shrink the segment and insert a new segment before it with the same people
+            segments.insert(i, [segments[i][0], point, copy(segments[i][2]), copy(segments[i][3])])
             segments[i + 1][0] = point + 1
             break
 
-linkage = {
-     '1': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '2': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '3': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '4': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '5': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '6': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '7': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '8': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     '9': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '10': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '11': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '12': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '13': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '14': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '15': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '16': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '17': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '18': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '19': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '20': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '21': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-    '22': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-     'X': [[0, sys.maxsize, int(proband_is_case), int(not proband_is_case)]],
-}
+if proband_is_case:
+    cases = ['Proband']
+    controls = []
+else:
+    cases = []
+    controls = ['Proband']
 
-cases = int(proband_is_case)
-controls = int(not proband_is_case)
+linkage = {
+     '1': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '2': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '3': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '4': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '5': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '6': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '7': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '8': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     '9': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '10': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '11': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '12': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '13': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '14': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '15': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '16': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '17': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '18': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '19': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '20': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '21': [[0, sys.maxsize, copy(cases), copy(controls)]],
+    '22': [[0, sys.maxsize, copy(cases), copy(controls)]],
+     'X': [[0, sys.maxsize, copy(cases), copy(controls)]],
+}
 
 def load_files(csv_dir, is_case):
     global cases
     global controls
     for filename in glob.glob(csv_dir + '/*.csv'):
+        person_name = splitext(basename(filename))[0]
         for row in csv.reader(open(filename, 'r')):
             if row == ['Comparison', 'Chromosome', 'Start Point', 'End Point', 'Genetic Distance', '#SNPs']:
                 continue
@@ -124,13 +135,13 @@ def load_files(csv_dir, is_case):
             for segment in linkage[chromosome]:
                 if start_point <= segment[0] and segment[1] <= end_point:
                     if is_case:
-                        segment[2] += 1
+                        segment[2].append(person_name)
                     else:
-                        segment[3] += 1
+                        segment[3].append(person_name)
         if is_case:
-            cases += 1
+            cases.append(person_name)
         else:
-            controls += 1
+            controls.append(person_name)
 
 load_files(case_dir, True)
 load_files(control_dir, False)
@@ -139,9 +150,9 @@ total_segments = 0
 for chromosome, segments in linkage.items():
     total_segments += len(segments)
 
-print('Comparing ' + str(total_segments) + ' segments between ' + str(cases) + ' cases and ' + str(controls) + ' controls.')
+print('Comparing ' + str(total_segments) + ' segments between ' + str(len(cases)) + ' cases and ' + str(len(controls)) + ' controls.')
 
-if cases < 10 or controls < 10:
+if len(cases) < 10 or len(controls) < 10:
     print('WARNING: You have less than 10 cases or less than 10 controls.')
     print('The p values output by this program are not accurate.')
 
@@ -158,22 +169,43 @@ for chromosome, segments in sorted(linkage.items(), key=pad_key):
         if segment[0] == 0 or segment[1] == sys.maxsize:
             continue
         try:
-            p = (segment[2] + segment[3]) / (cases + controls)
-            se = sqrt(p * (1 - p) * (1 / cases + 1 / controls))
-            z = -abs((segment[2] / cases - segment[3] / controls) / se)
+            p = (len(segment[2]) + len(segment[3])) / (len(cases) + len(controls))
+            se = sqrt(p * (1 - p) * (1 / len(cases) + 1 / len(controls)))
+            z = -abs((len(segment[2]) / len(cases) - len(segment[3]) / len(controls)) / se)
             p = 2 * norm.cdf(z)
             if p <= alpha / m:
                 if not difference_found:
-                    print('Chromosome\tStart\tEnd\tCase freq\tControl freq\tp')
+                    header = 'Chromosome\tStart\tEnd\tCase freq\tControl freq\tp'
+                    if want_misfits:
+                        header += '\tCase misfits\tControl misfits'
+                    print(header)
                     difference_found = True
-                print(
+
+                case_freq = len(segment[2]) / len(cases)
+                control_freq = len(segment[3]) / len(controls)
+                if case_freq >= control_freq:
+                    #we expect the cases to have the allele and the controls to not have it
+                    case_misfits = set(cases).difference(set(segment[2]))
+                    control_misfits = set(segment[3])
+                else:
+                    #we expect the cases to not have the allele and the controls to have it
+                    case_misfits = set(segment[2])
+                    control_misfits = set(controls).difference(set(segment[3]))
+
+                output = (
                     str(chromosome) + '\t' +
                     str(segment[0]) + '\t' +
                     str(segment[1]) + '\t' +
-                    str(segment[2] / cases) + '\t' +
-                    str(segment[3] / controls) + '\t' +
+                    str(case_freq) + '\t' +
+                    str(control_freq) + '\t' +
                     str(p)
                 )
+                if want_misfits:
+                    output += (
+                        '\t' + str(case_misfits) +
+                        '\t' + str(control_misfits)
+                    )
+                print(output)
         except ZeroDivisionError:
             pass
 
