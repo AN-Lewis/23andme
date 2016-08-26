db2e3434fedb374a840f2a5059300ff1c05b08f8
Alex Henrie <alexhenrie24@gmail.com>
2016-08-25 21:42:23 -0600

    proband-linkage: Use Fisher's exact test when appropriate

--- proband-linkage/proband-linkage.py
+++ proband-linkage/proband-linkage.py
@@ -30,7 +30,9 @@ from getopt import getopt
 from math import sqrt
 from os.path import basename
 from os.path import splitext
-from scipy.stats import norm
+from scipy.stats import chi2_contingency
+from scipy.stats import fisher_exact
+from scipy.stats.contingency import expected_freq
 from sys import stderr
 
 case_dir = 'cases'
@@ -39,11 +41,13 @@ recursive = False
 proband_is_case = None
 alpha = 0.05
 m = 23
+method = 'auto'
+yates = True
 want_misfits = False
 
 optlist, args = getopt(
     sys.argv[1:], '-ar',
-    ['cases=', 'controls=', 'proband=', 'alpha=', 'recursive', 'no-bonferroni', 'misfits', 'help']
+    ['cases=', 'controls=', 'proband=', 'alpha=', 'recursive', 'no-bonferroni', 'method=', 'no-yates', 'misfits', 'help']
 )
 for name, value in optlist:
     if name == '--cases':
@@ -61,13 +65,19 @@ for name, value in optlist:
         alpha = float(value)
     elif name == '--no-bonferroni':
         m = 1
+    elif name == '--method':
+        method = value
+    elif name == '--no-yates':
+        yates = False
     elif name == '--misfits':
         want_misfits = True
     elif name == '--help':
         print('Syntax: ./proband-linkage.py [--cases=<dir>] [--controls=<dir>]')
         print('                [-r | --recursive] [--proband=(case|control)]')
-        print('                [-a <value> | --alpha=<value>] [--no-bonferroni] [--misfits]')
-        print('cases defaults to ./cases, controls defaults to ./controls, and alpha defaults to 0.05.')
+        print('                [-a <value> | --alpha=<value>] [--no-bonferroni]')
+        print('                [--method=(chi|fisher|auto)] [--no-yates] [--misfits]')
+        print('cases defaults to ./cases, controls defaults to ./controls, alpha defaults to')
+        print('0.05, and method defaults to auto.')
         exit()
 
 while proband_is_case == None:
@@ -164,10 +174,6 @@ for chromosome, segments in linkage.items():
 
 stderr.write('Comparing ' + str(total_segments) + ' segments between ' + str(len(cases)) + ' cases and ' + str(len(controls)) + ' controls.\n')
 
-if len(cases) < 10 or len(controls) < 10:
-    stderr.write('WARNING: You have less than 10 cases or less than 10 controls.\n')
-    stderr.write('The p values output by this program are not accurate.\n')
-
 difference_found = False
 
 def pad_key(pair):
@@ -181,13 +187,45 @@ for chromosome, segments in sorted(linkage.items(), key=pad_key):
         if segment[0] == 0 or segment[1] == sys.maxsize:
             continue
         try:
-            p = (len(segment[2]) + len(segment[3])) / (len(cases) + len(controls))
-            se = sqrt(p * (1 - p) * (1 / len(cases) + 1 / len(controls)))
-            z = -abs((len(segment[2]) / len(cases) - len(segment[3]) / len(controls)) / se)
-            p = 2 * norm.cdf(z)
+            contingency_table = [
+                #Cases with segment            Controls with segment
+                [len(segment[2]),              len(segment[3])],
+                #Cases without segment         Controls without segment
+                [len(cases) - len(segment[2]), len(controls) - len(segment[3])]
+            ]
+            if method == 'chi':
+                p = chi2_contingency(contingency_table, correction=yates)[1]
+                if yates:
+                    method_name = 'Yates chi-squared'
+                else:
+                    method_name = 'Chi-squared'
+            elif method == 'fisher':
+                p = fisher_exact(contingency_table)[1]
+                method_name = 'Fisher'
+            else:
+                expected_frequency_table = expected_freq(contingency_table)
+                num_large_cells = 0
+                num_small_cells = 0
+                for row in contingency_table:
+                    for cell in row:
+                        if cell >= 5:
+                            num_large_cells += 1
+                        elif cell < 1:
+                            num_small_cells += 1
+                            break
+                if num_large_cells >= 3 and num_small_cells == 0:
+                    p = chi2_contingency(contingency_table, correction=yates)[1]
+                    if yates:
+                        method_name = 'Yates chi-squared'
+                    else:
+                        method_name = 'Chi-squared'
+                else:
+                    p = fisher_exact(contingency_table)[1]
+                    method_name = 'Fisher'
+
             if p <= alpha / m:
                 if not difference_found:
-                    header = 'Chromosome\tStart\tEnd\tCase freq\tControl freq\tp'
+                    header = 'Chromosome\tStart\tEnd\tCase freq\tControl freq\tp\tMethod'
                     if want_misfits:
                         header += '\tCase misfits\tControl misfits'
                     print(header)
@@ -210,7 +248,8 @@ for chromosome, segments in sorted(linkage.items(), key=pad_key):
                     str(segment[1]) + '\t' +
                     str(case_freq) + '\t' +
                     str(control_freq) + '\t' +
-                    str(p)
+                    str(p) + '\t' +
+                    method_name
                 )
                 if want_misfits:
                     output += (
